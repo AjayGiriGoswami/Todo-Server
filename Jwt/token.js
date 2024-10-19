c599c3e7b7fa0407c1f4b8f5fee1b59baede9b1a
AjayGiriGoswami <153065760+AjayGiriGoswami@users.noreply.github.com>
2024-10-19 11:01:28 +0530

    Update token.js

--- Jwt/token.js
+++ Jwt/token.js
@@ -2,13 +2,11 @@ import jwt from "jsonwebtoken";
 import userModel from "../Models/UserModel.js";
 
 export const jwttoken = async (userId, res) => {
-  const token = await jwt.sign({ userId }, process.env.KEY, {
+  const token = jwt.sign({ userId }, process.env.KEY, {
     expiresIn: "10d",
   });
   res.cookie ("jwt",token ,{
     httpOnly: true,
-    secure: false,
-    sameSite : "lax",
     path: "/"
   })
 
